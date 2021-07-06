a8e37f99fd5b874067fe35d62bf1a8201d5cb812
Petter Sjunnestrand <p.sjunnestrand@gmail.com>
2021-07-06 21:55:37 +0200

    initial commit

---
+++ src/heart.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Heart extends Component {
+    render () {
+        return (
+            <div>
+                <div id="heart"></div>
+                <div id="heartBackground"></div>
+            </div>
+        )
+    }
+}
+
+export default Heart;
